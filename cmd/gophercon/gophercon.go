85aacb579fa125944a5b179c75eb43a07514a684
Flávio Diniz <flavio.diniz@olx.com>
2018-05-31 11:29:30 +0100

    Create first route to process /home

---
+++ cmd/gophercon/gophercon.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// go run ./cmd/gophercon/gophercon.go
+// curl -i http://127.0.0.1:8000/home
+func main() {
+	log.Printf("Service is starting...")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/home", homeHandler()).Methods(http.MethodGet)
+
+	http.ListenAndServe(":8000", r)
+}
+
+func homeHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request is processing: %s", r.URL.Path)
+		fmt.Fprint(w, "Hello! Your request was processed")
+	}
+}
